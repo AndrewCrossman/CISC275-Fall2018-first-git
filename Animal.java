49df52a70e607cd869146012c163078fc819f3aa
Andrew Crossman <crossman@udel.edu>
2018-09-17 18:36:31 -0400

    Version Two

--- Animal.java
+++ Animal.java
@@ -1,8 +1,11 @@
 abstract class Animal{
 	int legs;
-	string name;
+	String name;
 	public Animal(String name, int legs){
 		this.name=name;
 		this.legs=legs;
+	}
+	public int getLegs(){
+		return(legs)
 	}
 }
